2cd95b2a05d7c6d7af8578da2c6fa2320f674ccf
deepakappways <deepakb@waysapp.in>
2019-06-08 16:04:12 +0530

    Add Save Button in Layout and AttachListener with Button

--- app/src/main/java/com/example/keyboardshortcut/MainActivity.kt
+++ app/src/main/java/com/example/keyboardshortcut/MainActivity.kt
@@ -1,14 +1,32 @@
 package com.example.keyboardshortcut
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.keyboardshortcut.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.log
 
-class MainActivity : BaseActivity() {
+class MainActivity : BaseActivity(),View.OnClickListener {
+    override fun onClick(p0: View?) {
+        when(p0!!.id)
+        {
+            R.id.btn_save-> Toast.makeText(
+                this,
+                    "Save Clicked",
+                    Toast.LENGTH_LONG
+                ).show()
+            R.id.bt_addkey-> Toast.makeText(this,"Add Key Clicked",Toast.LENGTH_LONG).show()
+        }
+    }
 
+    lateinit var activityMainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        activityMainBinding.onClickListener = this
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
