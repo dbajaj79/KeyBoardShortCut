2454b3fecbe73302677949752760adb230a94be0
deepakappways <deepakb@waysapp.in>
2019-06-08 13:46:43 +0530

    Implement DataBinding and Create UI of App

---
+++ app/src/main/java/com/example/keyboardshortcut/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.keyboardshortcut
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+}
